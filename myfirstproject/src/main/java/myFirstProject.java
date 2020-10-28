4a519669dcba0cdc339fe02cacbe58762e25a7a1
Dmitriy <pikhtelev.dmitriy@gmail.com>
2020-10-27 19:44:16 -0500

    my first project

---
+++ myfirstproject/src/main/java/myFirstProject.java
@@ -0,0 +1,11 @@
+public class myFirstProject {
+    public static void main(String[] args) {
+        String firstName="Dmitriy";
+        String lastName="Pikhtelev";
+        System.out.println("Hello, World!");
+        System.out.println("========================");
+        System.out.println("My name is" + " " +firstName + " " +lastName + ".");
+        System.out.println("========================");
+    }
+
+}
